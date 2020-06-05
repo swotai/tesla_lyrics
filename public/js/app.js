8e17720df29142bc8fb1771d6b4f42884c68449e
Dennis Tai <swotai@gmail.com>
2020-06-05 16:57:49 -0700

    Small success!
    successfully show some lyrics!

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,118 @@
-window.addEventListener('load', () => {
-    const router = new Router({
-        mode: 'history',
-        page
-    })
+window.addEventListener("load", () => {
+  const el = $("#app");
+
+  // Compile Handlebar Templates
+  const errorTemplate = Handlebars.compile($("#error-template").html());
+  const lyricsTemplate = Handlebars.compile($("#lyrics-template").html());
+  const loginTemplate = Handlebars.compile($("#login-template").html());
+
+  // Router Declaration
+  const router = new Router({
+    mode: "history",
+    page404: (path) => {
+      const html = errorTemplate({
+        color: "yellow",
+        title: "Error 404 - Page NOT Found!",
+        message: `The path '/${path}' does not exist on this site`,
+      });
+      el.html(html);
+    },
+  });
+
+  // Instantiate api handler
+  const api = axios.create({
+    baseURL: `${window.location.protocol}//${window.location.host}/api`,
+    timeout: 5000,
+  });
+
+  // Display Error Banner
+  const showError = (error) => {
+    const { title, message } = error.response.data;
+    const html = errorTemplate({ color: 'red', title, message });
+    el.html(html);
+  };
+
+  // lyrics page and aux functions
+  const getLyricsResults = async () => {
+    // get params from web form
+    const song = $('#song').val();
+    const artist = $('#artist').val();
+    // send post data for lyrics
+    try {
+      const response = await api.post('/lyrics', { song, artist });
+      const { name, author, album, lrc } = response.data;
+      let html = lyricsTemplate({ name, author, album, lrc });
+      el.html(html);
+    } catch (error) {
+      showError(error)
+    } finally {
+      $('.loading').removeClass('loading');
+    }
+  }
+
+  // Handle Convert Button Click Event
+  const getLyricsHandler = () => {
+    if ($('.ui.form').form('is valid')) {
+      // hide error message
+      $('.ui.error.message').hide();
+      // Post to Express server
+      $('#lyrics-page').addClass('loading');
+      getLyricsResults();
+      // Prevent page from submitting to server
+      return false;
+    }
+    return true;
+  };
+
+  router.add("/", () => {
+    let html = lyricsTemplate();
+    el.html(html);
+    try {
+      // Validate Form Inputs
+      $('.ui.form').form({
+        fields: {
+          song: 'empty',
+        },
+      });
+      // Specify Submit Handler
+      $('.submit').click(getLyricsHandler);
+    } catch (error) {
+      showError(error)
+    }
+  });
+
+
+  // spotify login page
+
+  router.add("/login", () => {
+    let html = loginTemplate();
+    el.html(html);
+  });
+
+//   router.add("/historical", () => {
+//     let html = historicalTemplate();
+//     el.html(html);
+//   });
+
+  // Navigate app to current url
+  router.navigateTo(window.location.pathname);
+
+  // Highlight Active Menu on Refresh/Page Reload
+  const link = $(`a[href$='${window.location.pathname}']`);
+  link.addClass("active");
+
+  $("a").on("click", (event) => {
+    // Block browser page load
+    event.preventDefault();
+
+    // Highlight Active Menu on Click
+    const target = $(event.target);
+    $(".item").removeClass("active");
+    target.addClass("active");
+
+    // Navigate to clicked url
+    const href = target.attr("href");
+    const path = href.substr(href.lastIndexOf("/"));
+    router.navigateTo(path);
+  });
 });
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('dotenv').config(); // read .env files
 const express = require('express');
+const bodyParser = require('body-parser');
+const { getLyrics } = require("./lib/guaqb-service");
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -10,39 +12,56 @@ app.use(express.static('public'));
 // Allow front-end access to node_modules folder
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
+// Parse POST data as URL encoded data
+app.use(bodyParser.urlencoded({ extended: true, }));
+
+// Parse POST data as JSON
+app.use(bodyParser.json());
+
+// Express Error handler
+const errorHandler = (err, req, res) => {
+    if (err.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        res.status(403).send({ title: 'Server responded with an error', message: err.message });
+    } else if (err.request) {
+        // The request was made but no response was received
+        res.status(503).send({ title: 'Unable to communicate with server', message: err.message });
+    } else {
+        // Something happened in setting up the request that triggered an Error
+        res.status(500).send({ title: 'An unexpected error occurred', message: err.message });
+    }
+};
+
+// fetch lyrics
+app.post('/api/lyrics', async (req, res) => {
+    try {
+        const { song, artist } = req.body;
+        const data = await getLyrics(song, artist);
+        res.setHeader('Content-Type', 'application/json')
+        res.send(data[0])
+    } catch (err) {
+        errorHandler(err, req, res);
+    }
+});
+
 // Redirect all traffic to index.html
 app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
-
-// handles 404
-app.use(function (req, res, next) {
-    res.status(404);
-    // respond with html page
-    if (req.accepts('html')) {
-        res.render('404', { url: req.url });
-        return;
-    }
-    // respond with json
-    if (req.accepts('json')) {
-        res.send({ error: 'Not found' });
-        return;
-    }
-    // default to plain-text. send()
-    res.type('txt').send('Not found');
-});
 
 // Listen for HTTP requests on port
 app.listen(port, () => {
     console.log('listening on %d', port);
 });
 
-const { getLyrics, getLyrics1, getLyrics2 } = require('./lib/guaqb-service');
 const fs = require('fs');
 const test = () => {
+    const { getLyrics, getLyrics1, getLyrics2 } = require("./lib/guaqb-service");
+
     // var data = await getLyrics1('洋蔥');
     // console.log(data);
     // console.log('>>>>>>>>>>>>>>>>>>>');
     // var data2 = await getLyrics2();
-    // console.log(data2);
+    // console.log(data2);s
     // console.log('>>>>>>>>>>>>>>>>>>>');
     getLyrics('有一種悲傷', 'A-Lin')
     .then(d => {
@@ -55,4 +74,4 @@ const test = () => {
     
 };
 
-test();
+// test();
