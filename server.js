0d025405edc4423ee0be2338a66446c0d1f9d6e9
Dennis Tai <swotai@gmail.com>
2021-08-07 22:21:55 -0700

    gh-26 bugfix
    Pay the price of rebasing randomly...

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ const errorHandler = (err, req, res) => {
 };
 
 // service routes
-app.use("/lyrics_svc_1", require("./lib/guaqb-service").lyrics_router);
+// app.use("/lyrics_svc_1", require("./lib/lyrics-service").lyrics_router);
 app.use("/spotify", require("./lib/spotify-service").spotify_router);
 app.use("/lyrics_svc", require("./lib/lyrics-service-simple").lyrics_router);
 
